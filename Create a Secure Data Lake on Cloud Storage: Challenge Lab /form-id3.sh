bfda90b0db67b849d240b26de3cd966bb7fee95a
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2025-11-25 14:22:28 +0530

    Create form-id3.sh

---
+++ Create a Secure Data Lake on Cloud Storage: Challenge Lab /form-id3.sh
@@ -0,0 +1,23 @@
+### ✅ Form 3: BigQuery Dataset
+
+- **Create a BigQuery dataset**
+- **Add a zone to your lake**
+- **Attach an existing BigQuery Dataset to the Lake**
+- **Create a tag template (BigQuery Dataset)**
+
+
+
+---
+### copy the command from here 
+
+```
+export ZONE=
+```
+`````
+curl -LO https://raw.githubusercontent.com/chayandeokar/GCP-Cloud-Solutions/refs/heads/master/Create%20a%20Secure%20Data%20Lake%20on%20Cloud%20Storage%3A%20Challenge%20Lab%20/chayan-3.sh
+sudo chmod +x chayan-3.sh
+./chayan-3.sh
+`````
+
+
+### Congratulations !!!!
