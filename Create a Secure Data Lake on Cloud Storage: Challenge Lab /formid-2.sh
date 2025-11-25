21976e463828349464c7188932a6a9ad167d2bc8
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2025-11-25 14:55:28 +0530

    Update formid-2.sh

--- Create a Secure Data Lake on Cloud Storage: Challenge Lab /formid-2.sh
+++ Create a Secure Data Lake on Cloud Storage: Challenge Lab /formid-2.sh
@@ -11,6 +11,6 @@ export ZONE=
 ```
 `````
 curl -LO https://raw.githubusercontent.com/chayandeokar/GCP-Cloud-Solutions/refs/heads/master/Create%20a%20Secure%20Data%20Lake%20on%20Cloud%20Storage%3A%20Challenge%20Lab%20/chayan-2.sh
-sudo chmod +x Achayan-2.sh
+sudo chmod +x chayan-2.sh
 ./chayan-2.sh
 `````
