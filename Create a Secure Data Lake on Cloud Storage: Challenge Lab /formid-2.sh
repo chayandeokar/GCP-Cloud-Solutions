adcf494c1527e63e83a05ddf8d661523ac1e90ff
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2025-11-25 14:28:08 +0530

    Create formid-2.sh

---
+++ Create a Secure Data Lake on Cloud Storage: Challenge Lab /formid-2.sh
@@ -0,0 +1,16 @@
+### ✅ Form 2: Existing Storage Bucket + Lake
+
+- **Create a lake in Dataplex and add a zone to your lake**
+- **Create an entry group**
+- **Attach an existing Cloud Storage bucket to the zone**
+- **Create a tag template (Storage bucket)**
+
+---
+```
+export ZONE=
+```
+`````
+curl -LO https://raw.githubusercontent.com/chayandeokar/GCP-Cloud-Solutions/refs/heads/master/Create%20a%20Secure%20Data%20Lake%20on%20Cloud%20Storage%3A%20Challenge%20Lab%20/chayan-2.sh
+sudo chmod +x Achayan-2.sh
+./chayan-2.sh
+`````
