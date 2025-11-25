d7f33ced2e225da5377da022e0ef47d92b0b3ce8
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2025-11-25 14:24:56 +0530

    Create Form-3id.sh

---
+++ Create a Secure Data Lake on Cloud Storage: Challenge Lab /Form-3id.sh
@@ -0,0 +1,17 @@
+## 🧩 Form Details
+
+### ✅ Form 1: Cloud Storage Bucket + New Lake
+
+- **Create a Cloud Storage bucket**
+- **Create a lake in Dataplex and add a zone to your lake**
+- **Create an entry group**
+- **Create a tag template (Storage bucket)**
+---
+``````
+export ZONE=
+``````
+`````
+curl -LO https://raw.githubusercontent.com/chayandeokar/GCP-Cloud-Solutions/refs/heads/master/Create%20a%20Secure%20Data%20Lake%20on%20Cloud%20Storage%3A%20Challenge%20Lab%20/chayan-3.sh
+sudo chmod +x chayan-3.sh
+./chayan-3.sh
+`````
