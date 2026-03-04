3d9fac13526e011d1f579e12b47e7224b0099257
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2026-03-04 18:16:40 +0530

    Update chayan.sh

--- Cloud Storage: Qwik Start - Cloud Console /chayan.sh
+++ Cloud Storage: Qwik Start - Cloud Console /chayan.sh
@@ -2,7 +2,7 @@ export PROJECT_ID=$(gcloud config get-value project)
 
 gsutil mb -l $REGION -c Standard gs://$PROJECT_ID
 
-curl -O https://github.com/Itsabhishek7py/GoogleCloudSkillsboost/blob/f31d8eff4b56e6b72b256884cf4fc62d17fc58fa/Cloud%20Storage%3A%20Qwik%20Start%20-%20Cloud%20Console/kitten.png
+curl -O https://github.com/chayandeokar/GCP-Cloud-Solutions/blob/master/Cloud%20Storage%3A%20Qwik%20Start%20-%20Cloud%20Console/kitten.png
 
 gsutil cp kitten.png gs://$PROJECT_ID/kitten.png
 
